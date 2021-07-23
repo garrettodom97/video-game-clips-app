977e44ae512983258c25a056bcafc02d33668391
Garrett <garrettodom97@gmail.com>
2021-07-23 15:58:32 -0500

    Initial commit with models for users, comments, and posts along with some seed data and model associations

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+User.create!(username: "grandtheftodom", password_digest: "password")
+User.create!(username: "First Persian", password_digest: "password")
+
+Post.create!(title: "Test", description: "test post", video: "url", user_id: 1)
+Post.create!(title: "Cool video", description: "this video rocks", video: "url", user_id: 2)
+Post.create!(title: "Anotha One", description: "this video ALSO rocks", video: "url", user_id: 2)
+
+Comment.create!(text: "nice dog", user_id: 1, post_id: 2)
+Comment.create!(text: "ALSO nice dog", user_id: 1, post_id: 3)
+Comment.create!(text: "sick test dog", user_id: 2, post_id: 1)
