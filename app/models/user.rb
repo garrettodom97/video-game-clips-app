551e3d33249ae7aa89f1ea6627e109fa89781614
Garrett <garrettodom97@gmail.com>
2021-08-18 14:50:26 -0500

    adds created at attribute to comments serializer and formats it in comments model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
 
+  def created_at
+    attributes["created_at"].strftime("%-I:%M %p %m/%d/%Y ")
+  end
+
   has_many :posts
   has_many :comments
 end
