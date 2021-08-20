0bfcc76124e9061c06a64c90e5f31239194bbc38
Garrett <garrettodom97@gmail.com>
2021-08-20 12:38:45 -0500

    formats comment date and time

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
   validates :text, presence: true
 
+  def created_at
+    attributes["created_at"].strftime("%-I:%M %p %m/%d/%Y ")
+  end
+
   belongs_to :user
   belongs_to :post
 end
